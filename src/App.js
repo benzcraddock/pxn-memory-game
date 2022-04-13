c91660b5409cd7569604f213f5c43f67fa1f159a
Benjamin Craddock <benjamincraddock@benjamins-mbp.lan>
2022-04-13 01:26:17 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import SingleCard from './components/SingleCard';
+
+const cardImages = [
+  { 'src': '/images/pxn1.png', matched: false },
+  { 'src': '/images/pxn2.jpeg', matched: false },
+  { 'src': '/images/pxn3.gif', matched: false },
+  { 'src': '/images/pxn4.gif', matched: false },
+  { 'src': '/images/pxn5.gif', matched: false },
+  { 'src': '/images/pxn6.png', matched: false },
+  { 'src': '/images/pxn9.png', matched: false },
+  { 'src': '/images/pxn15.png', matched: false },
+]
 
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  // choices
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+  
+  // disable
+  const [disabled, setDisabled] = useState(false)
+
+  // shuffle cards when new game is clicked
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({...card, id: Math.random()}))
+
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
+  // handle choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  // compare two selected cards
+  useEffect(() => {
+    if(choiceOne && choiceTwo) {
+      setDisabled(true)
+      if(choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if(card.src === choiceOne.src) {
+              return {...card, matched: true}
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        setTimeout(() => resetTurn(), 1000)
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+  // reset choices and increase turn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
+  }
+
+  // start new game automatically
+  useEffect(() => {
+    shuffleCards()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Let's Play Memory PXN for GHOSTS</h1>
+      <h2>a mini game created by Benye (benyetothe#1279)</h2>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className='card-grid'>
+        {cards.map(card => (
+          <SingleCard 
+            key={card.id} 
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+      <p>Your Ghosts Count: {turns}</p>
     </div>
   );
 }
---
+++ src/components/SingleCard.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './SingleCard.css'
+
+export default function SingleCard({ card, handleChoice, flipped, disabled }) {
+
+  const handleClick = () => {
+    if(!disabled) {
+      handleChoice(card)
+    }
+  }
+
+  return (
+    <div className='card'>
+      <div className={flipped ? 'flipped' : ''}>
+        <img className='front' src={card.src} alt='card-front'/>
+        <img 
+          className='back' 
+          onClick={handleClick} 
+          style={{cursor:'pointer'}}
+          src='/images/pxn-logo.jpg' 
+          alt='card-back'/>
+      </div>
+    </div>
+  )
+}
